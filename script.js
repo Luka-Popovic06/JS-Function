ec0ceb60486359aa1d304af520d4086a9d309d08
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-13 12:10:07 +0100

    Creating variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+/* Zadatak-1 */
+const naslov = "JavaScript za početnike";
+const autor = "Ivan Ivić";
+const godinaIzdavanja = 2022;
+const cena = 25;
+const stanje = "nova";
+const zanr = "tehnologija";
+const naStanju = true;
